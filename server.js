dfc7a818eb346340e1665aa2ad7c9f683c6aa01d
bhara <bharathJ222000@gmail.com>
2021-01-10 12:48:51 +0530

    server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 
 app.use('/api/transactions', require('./routes/transactions'));
@@ -24,15 +24,15 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode in port ${PORT}`.blue.bold
-  )
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode in port ${PORT}`.blue.bold
+    )
 );
